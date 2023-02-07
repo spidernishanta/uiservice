e3deae5c2e486b5cfef66ab580750bdc03063e63
Jintu-png <60797876+Jintu-png@users.noreply.github.com>
2023-02-07 23:51:59 +0530

    Changes in paymentReport

--- storeflex/src/pages/paymentReport.tsx
+++ storeflex/src/pages/paymentReport.tsx
@@ -1,51 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Box, Grid } from '@mui/material';
 import TopNavBar from '../components/navbar/TopNavBar';
 import SideNavBar from '../components/navbar/SideNavBar';
 import { AppContainer, SplitPaneContainer } from '../components/containers/containers';
 import Footer from '../components/footer/footer';
 import { getUserType } from '../utils/CommonUtils';
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid  } from "@mui/x-data-grid";
 import InputBox from '../components/atoms/textfield/InputBox';
+import { UserType } from '../components/atoms/adduser/UserHelper';
+import { UserPostData } from '../api/ApiConfig';
+import GetCompany from '../components/atoms/company/GetCompany';
+import { Balance, CallReceived } from '@mui/icons-material';
 
 const PaymentReport = () => {
 
+    
+
+    const [userPostInfo, setUserPostInfo] = useState<UserPostData>();
+    const onCompanyChange = (id: string) => {
+        setUserPostInfo({ ...userPostInfo, clientId: id });
+        console.log(' << onCompanyChange >> ', id);
+    }
+
     const columns = [
-        // {
-        //   field: "actions",
-        //   headerName: "ACTIONS",
-        //   width: 100,
-        //   sortable: false,
-        //   disableColumnMenu: true,
-        //   renderCell: (params) => {
-        //     return (
-        //       <Box
-        //         sx={{
-        //           width: "100%",
-        //           height: "100%",
-        //           display: "flex",
-        //           justifyContent: "center",
-        //           alignItems: "center",
-        //         }}
-        //       >
-        //       </Box>
-        //     );
-        //   },
-        // },
+
         { field: "orderId", headerName: "Order Id", width: 80 },
         { field: "monthYear", headerName: "Month/Year", width: 100 },
         { field: "qty", headerName: "Qty", width: 100 },
         { field: "unitPrice", headerName: "Unit Price", width: 100 },
         { field: "price", headerName: "Price", width: 100 },
         { field: "tax", headerName: "Tax", width: 100 },
-        { field: "totalPrice", headerName: "Total Price", width: 100 },
-        { field: "paidAmount", headerName: "Paid Amount", width: 100 },
-        { field: "balance", headerName: "Balance", width: 100 },
+        { field: "totalPrice", headerName: "Total Price", width: 100 , },
+        { field: "amountRecieved", headerName: "Amount Recieved", width: 130, editable: true },
+        { field: "balance", headerName: "Balance", width: 100, aggfunc:"sum" },
         { field: "status", headerName: "Status", width: 100 },
-        { field: "payout", headerName: "Payout", witdh: 100},
-        { field: "paid", headerName: "Paid", witdh: 100, editable: true},
-        { field: "balPayout", headerName: "Bal Payout", witdh: 100},
-      ];
+        { field: "number", headerName: "Number", witdh: 100 },
+        { field: "paid", headerName: "Paid", witdh: 100, editable: true },
+        { field: "balPayout", headerName: "Bal Payout", witdh: 100 },
+    
+    ];
+
 
     return (
         <AppContainer>
@@ -62,14 +56,16 @@ const PaymentReport = () => {
                             </div>
                             <Grid container spacing={2} sx={{ mt: 0 }}>
                                 <Grid item xs={6}>
-                                <InputBox data={{
-                                            name: 'Warehouse_name', label: 'Warehouse Name',
-                                            value: ''
-                                        }}
-                                        />
+                                    <p>Warhouse Name</p>
+                                    <div className='p-top-md'>
+                                        {<GetCompany onCompanyChange={onCompanyChange} />}
+                                    </div>
                                 </Grid>
                                 <Grid item xs={6}>
                                     <p>Order:</p>
+                                    <div className='p-top-md'>
+                                        {<GetCompany onCompanyChange={onCompanyChange} />}
+                                    </div>
                                 </Grid>
                             </Grid>
                             <Grid container spacing={2} sx={{ mt: 1 }}>
@@ -91,10 +87,12 @@ const PaymentReport = () => {
                             <div style={{ height: 370, width: "100%" }}>
                                 <DataGrid getRowHeight={() => 'auto'}
                                     rows={[
-                                        {id:'1', orderId: 'WH1', monthYear: '12/2022', qty: '50', unitPrice: 'Rs.400', price: 'Rs.25,000', tax: 'Rs.1,000', totalPrice: '$90 million USD', paidAmount:'Rs.2000' ,balance: 'Rs.5000', status:'Active', payout:'NA',paid:'NA', balPayout:'Rs.5000'},
-                                        {id:'2', orderId: 'WH87', monthYear: '12/2020', qty: '100', unitPrice: 'Rs.1000', price: 'Rs.25,000', tax: 'Rs.1,000', totalPrice: '$90 million USD', paidAmount:'Rs.10000' ,balance: 'NA', status:'Close', payout:'NA',paid:'NA', balPayout:'NA'},
-                                        {id:'3', orderId: 'WH3', monthYear: '01/2019', qty: '2000', unitPrice: 'Rs.20000', price: 'Rs.25,000', tax: 'Rs.1,000', totalPrice: '$90 million USD', paidAmount:'Rs.10000' ,balance: 'Rs.6000', status:'Active', payout:'NA',paid:'NA', balPayout:'Rs.6000'},
-                                        ]}
+                                        
+                                        
+                                        { id: '1', orderId: 'WH1', monthYear: '12/2022', qty: '50', unitPrice: 'Rs.400', price: 'Rs.25,000', tax: 'Rs.1,000', totalPrice: 'Rs.90', amountRecieved: '', balance: '', status: '', payout: '', paid: '', balPayout: 'Rs.5000' },
+                                        { id: '2', orderId: 'WH87', monthYear: '12/2020', qty: '100', unitPrice: 'Rs.1000', price: 'Rs.25,000', tax: 'Rs.1,000', totalPrice: 'Rs.90', amountRecieved: '', balance: '', status: '', payout: '', paid: '', balPayout: '' },
+                                        { id: '3', orderId: 'WH3', monthYear: '01/2019', qty: '2000', unitPrice: 'Rs.20000', price: 'Rs.25,000', tax: 'Rs.1,000', totalPrice: 'Rs.90', amountRecieved: '', balance: '', status: '', payout: '', paid: '', balPayout: 'Rs.6000' },
+                                    ]}
                                     componentsProps={{}}
                                     columns={columns}
                                     pageSize={5}
