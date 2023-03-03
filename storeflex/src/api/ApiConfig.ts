39ca30822576b130843283a4afa4529a37fa18e9
ashutosh-m <ashu3234@gmail.com>
2023-03-03 15:32:24 -0500

    ac verification

--- storeflex/src/api/ApiConfig.ts
+++ storeflex/src/api/ApiConfig.ts
@@ -158,6 +158,8 @@ export interface SearchProps {
     search?: string;
 }
 
+// For the production environment: https://payout-api.cashfree.com
+// For the test environment: https://payout-gamma.cashfree.com
 export class ApiConfig {
     endPointGateWay = '/';
     testApi = '/test';
@@ -189,6 +191,7 @@ export class ApiConfig {
     getCompanyListApi = '/clientDropList';
     getWarehouseCategoriesApi = '/categories';
     getValidationBank = 'https://payout-api.cashfree.com/payout/v1/asyncValidation/bankDetails';
+    authorisationBank = 'http://payout-gamma.cashfree.com/payout/v1/authorize';
 
 }
 
